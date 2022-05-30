bbab3b254e6f165303e034151ff46848b245e13d
prince954 <akprince0268@gmail.com>
2022-05-31 00:42:06 +0600

    variable

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Procoder</title>
+</head>
+<body>
+    
+    <h3>Allah Mohan!</h3>
+
+     <?php
+       $food = "Banana";
+       $food_1 = " Apple";
+
+       echo $food . $food_1;
+        
+    ?>
+    
+</body>
+</html>

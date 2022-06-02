9e1d18b1d195840e92e14b3fffd81d1db1e8c793
prince954 <akprince0268@gmail.com>
2022-06-02 23:39:30 +0600

    function

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Procoder</title>
 </head>
-<body>
-    
 
-     <?php
+<body>
+
+
+    <?php
+    // echo "Allah Mohan" . "<br>";
+    /*
      //php Length
       echo strlen ('I Love Bangladesg' . "<br>");
 
@@ -25,7 +29,31 @@
         //php sentence word replace/word-change
         echo str_replace ('Pizza','Bargar','I Love to eat Pizza')
 
+        */
+
+
+    //    $t = date("H");
+
+    // if ($t < "20") {
+    //   echo "Have a good day!";
+    // }
+    // $a = 9;
+    // while ($a <= 10) {
+    //     echo "Alla Mohan" . "</br>";
+    //     $a++;
+    // }
+
+    function test_name($name, $age, $district)
+    {
+        echo "My Name is <b>$name</b>, His age is <b>$age</b> and District is <b>$district</b> </br>";
+    }
+    test_name("Prince", 18, "Jhenaidah");
+    test_name("Rakib", 20, "Dhaka");
+
+
     ?>
-    
+
+
 </body>
+
 </html>

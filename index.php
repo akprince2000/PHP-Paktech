65f4344c8b171b304bab97741d6be57df8724963
prince954 <akprince0268@gmail.com>
2022-06-03 20:33:43 +0600

    Switch Case

--- index.php
+++ index.php
@@ -12,29 +12,57 @@
 
 
     <?php
-    //cgpa Calclutor
+    //cgpa Calclutor if else
 
-    $marks = -1;
-    if ($marks >= 80 && $marks <= 100) {
-        echo "<h1> A+ </h1>";
-    } elseif ($marks >= 70 && $marks <= 79) {
-        echo "<h1> A </h1>";
-    } elseif ($marks >= 70 && $marks <= 79) {
-        echo "<h1> A- </h1>";
-    } elseif ($marks >= 60 && $marks <= 69) {
-        echo "<h1> B </h1>";
-    } elseif ($marks >= 50 && $marks <= 59) {
-        echo "<h1> B- </h1>";
-    } elseif ($marks >= 40 && $marks <= 49) {
-        echo "<h1> C </h1>";
-    } elseif ($marks >= 33 && $marks <= 39) {
-        echo "<h1> D </h1>";
-    } elseif ($marks >= 0 && $marks <= 32) {
-        echo "<h1> F </h1>";
-    } else {
-        echo "<h1>Invalide Number</h1>";
+
+    // $marks = 0;
+    // if ($marks >= 80 && $marks <= 100) {
+    //     echo "<h1> A+ </h1>";
+    // } elseif ($marks >= 70 && $marks <= 79) {
+    //     echo "<h1> A </h1>";
+    // } elseif ($marks >= 60 && $marks <= 69) {
+    //     echo "<h1> A- </h1>";
+    // } elseif ($marks >= 50 && $marks <= 59) {
+    //     echo "<h1> B </h1>";
+    // } elseif ($marks >= 40 && $marks <= 49) {
+    //     echo "<h1> C </h1>";
+    // } elseif ($marks >= 33 && $marks <= 39) {
+    //     echo "<h1> D </h1>";
+    // } elseif ($marks >= 0 && $marks <= 32) {
+    //     echo "<h1> F </h1>";
+    // } else {
+    //     echo "<h1>Invalide Number</h1>";
+    // }
+    ?>
+
+    <?php
+    // //switch case
+    $marks = 0;
+    switch (true) {
+        case ($marks >= 80 && $marks <= 100):
+            echo "<h1>A+</h1>";
+            break;
+        case ($marks >= 70 && $marks <= 79):
+            echo "<h1>A</h1>";
+            break;
+        case ($marks >= 60 && $marks <= 69):
+            echo "<h1>A-</h1>";
+            break;
+        case ($marks >= 50 && $marks <= 59):
+            echo "<h1>B</h1>";
+            break;
+        case ($marks >= 40 && $marks <= 49):
+            echo "<h1>C</h1>";
+            break;
+        case ($marks >= 33 && $marks <= 39):
+            echo "<h1>D</h1>";
+            break;
+        case ($marks >= 0 && $marks <= 32):
+            echo "<h1>F</h1>";
+            break;
+        default:
+            echo "Your Number is Rong";
     }
-
     ?>
 
 
